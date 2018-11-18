56edcef3812193823e2d194d5ae3c4fe5b349397
Mariya Lakevich <lakevich.maryia@gmail.com>
2018-11-18 17:01:17 +0300

    Page Factory/Page Object

---
+++ PageObject/PageObject/Pages/SearchPage.cs
@@ -0,0 +1,48 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace PageObject.Pages
+{
+    class SearchPage
+    {
+        [FindsBy(How = How.XPath, Using = "//div[.//input[@id='oneway']]//ins")]
+        public IWebElement oneWay { get; set; }
+
+        [FindsBy(How = How.Id, Using = "from_name")]
+        public IWebElement departure { get; set; }
+
+        [FindsBy(How = How.Id, Using = "to_name")]
+        public IWebElement arrival { get; set; }
+
+        [FindsBy(How = How.Id, Using = "departure_date")]
+        public IWebElement departureDate { get; set; }
+        [FindsBy(How = How.Id, Using = "ui-datepicker-div")]
+        private IWebElement _datePicker  { get; set; }
+
+        [FindsBy(How = How.ClassName, Using = "search_button")]
+        public IWebElement searchButton { get; set; }
+
+        public bool PickDate(DateTime date)
+        {
+            var weeks = _datePicker.FindElements(By.ClassName("day_td"));
+            foreach (var el in weeks)
+            {
+                if (el.GetAttribute("data-month") == (date.Month-1).ToString())
+                {
+                    var link = el.FindElement(By.ClassName("ui-state-default"));
+                    if (link.Text == date.Day.ToString())
+                    {
+                        link.Click();
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ PageObject/PageObject/Tests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+using PageObject.Pages;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace PageObject
+{
+    [TestFixture]
+    public class Test
+    {
+        IWebDriver driver;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            driver = new OpenQA.Selenium.Chrome.ChromeDriver();
+            driver.Manage().Window.Maximize();
+            // driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20); 
+
+        }
+
+        [OneTimeTearDown]
+        public void Cleanup()
+        {
+            driver.Quit();
+        }
+
+        [Test]
+        public void YesterdayDepartureDate()
+        {
+            driver.Url = "http://bilet.aviakassa.by/";
+            SearchPage searchPage = new SearchPage();
+            PageFactory.InitElements(driver, searchPage);
+
+            searchPage.oneWay.Click();
+
+            IWebElement searchForm = driver.FindElement(By.ClassName("inner-search"));
+
+            Thread.Sleep(1000);
+            searchForm = driver.FindElement(By.ClassName("new_search_form"));
+            searchPage.departure.Clear();
+            searchPage.departure.SendKeys("Лондон");
+            Thread.Sleep(1000);
+            searchPage.departure.SendKeys(Keys.Enter);
+            Thread.Sleep(1000);
+            searchPage.arrival.Clear();
+            searchPage.arrival.SendKeys("Москва");
+            Thread.Sleep(1000);
+            searchPage.arrival.SendKeys(Keys.Enter);
+
+            searchPage.departureDate.Click();
+            Thread.Sleep(1000);
+
+            Assert.AreEqual(false,searchPage.PickDate(DateTime.Now.AddDays(-1)));
+            
+        }
+    }
+}
